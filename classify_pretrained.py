59b4d0a4e3845449a668da836280e991db3ae01b
Thomas Astad Sve <Thomas.asve@gmail.com>
2016-06-10 10:46:39 +0200

    added classify images print and results print

--- classify_pretrained.py
+++ classify_pretrained.py
@@ -2,6 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import lasagne
 import googlenet
+import zipfile
+from random import shuffle
+from math import floor
+from preprocess_zip import load_zip_val_set
+from PIL import Image
+from StringIO import StringIO
+
 
 def load_pickle_googlenet():
     import pickle
@@ -32,7 +39,23 @@ def load_test_images(image_urls):
         images.append(image)
         
     return images, images_raw
-        
+
+def load_images(path, wnids, archive):
+    X = []
+    X_raw = []
+    val_annotations = path + "val_annotations.txt"    
+    for line in archive.open(val_annotations):
+        words = line.split()
+        img = archive.read(path + "images/" + words[0])
+        img = Image.open(StringIO(img))
+        image = np.array(img)
+        if image.ndim == 3:
+            X_raw.append(image)
+            image = np.rollaxis(image, 2)
+            X.append(image) # Append image to dataset
+            
+    return np.array(X, dtype=np.uint8), np.array(X_raw, dtype=np.uint8)
+
 def random_test_images(image_urls, num_samples = 5):
     np.random.seed(23)
     image_urls = image_urls[:num_samples]
@@ -40,10 +63,19 @@ def random_test_images(image_urls, num_samples = 5):
     
     return images, images_raw
 
+def load_classes_name(wnids, archive):
+    words = "tiny-imagenet-200/words.txt"
+    classes_words = {}
+    for line in archive.open(words):
+        words = line.split()
+        classes_words[words[0]] = words[1]
+
+    return classes_words
+
 def load_classes(wnid_file):
     return [line.strip() for line in open(wnid_file)]
 
-def print_predictions(images, images_raw, network, classes):
+def print_predictions(images, images_raw, network, classes, classes_words):
     for i in range(len(images)):
             prob = np.array(lasagne.layers.get_output(network, images[i], deterministic=True).eval())
             top5 = np.argsort(prob[0])[-1:-6:-1]
@@ -52,21 +84,44 @@ def print_predictions(images, images_raw, network, classes):
             plt.imread(images_raw[i].astype('uint8'))
             plt.axis('off')
             for n, label in enumerate(top5):
-                plt.text(250, 70 + n * 20, '{}. {}'.format(n+1, classes[label]), fontsize=14)
+                plt.text(250, 70 + n * 20, '{}. {}'.format(n+1, classes_words[classes[label]]), fontsize=14)
 
             plt.save("predicted_" + str(i) + ".JPEG")
             print "Saved plot: predicted_" + str(i) + ".JPEG"
             i = i + 1
                 
 def main():
-    wnid_file = "/home/thomas/data/dataset/tiny-imagenet-200/wnids.txt"
+    zip_url = "tiny-imagenet-200.zip"
+    wnid_file = "tiny-imagenet-200/wnids.txt"
     test_path = "tiny-imagenet-200/test/"
+    val_path = "tiny-imagenet-200/val/"
+
+
+    print "Reading from zip..."
+    archive = zipfile.ZipFile(zip_url, 'r')
+    wnids = [line.strip() for line in archive.open(wnid_file)] # Load list over classes
+    wnids = wnids[:100] # Load only the 100 first classes
+    classes_words = load_classes_name(wnids, archive)
     
     network = load_network()
     #network = load_pickle_googlenet()
-    images, images_raw = random_test_images()
-    classes = load_classes(wnid_file)
-    print_predictions(images, images_raw, network, classes)
+    #images, images_raw = random_test_images()
+    X, X_raw = load_images(val_path, wnids, archive)
 
+    data = zip(X, X_raw)
+    np.random.shuffle(data)
+    data = data[:5]
+    X, X_raw = zip(*data)
+        
+    classes = load_classes(wnid_file)
+    print_predictions(X, X_raw, network, classes, classes_words)
+
+    with load('googlenet_epochs.npz') as data:
+        results = data['results']
+
+    epoch_print = [1, 5, 10, 20, 30, 50, 100, 150, 200, 250]
+    for i in epoch_print:
+        print "Results, epoch " + str(i) + ": " + str(results[i - 1])
+    
 if __name__=="__main__":
     main()
